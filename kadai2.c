cf345aab6c9f216123f818af21f575216c2bf4fa
honokk <132962467+honokk@users.noreply.github.com>
2023-11-17 14:02:40 +0900

    Add files via upload

---
+++ kadai2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main (void){
+    int x = 10;
+    int y = x * 2;
+    int z = x + y;
+    printf("%d, %d, %d",x,y,z);
+}
---
+++ kadai3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (void){
+
+    int price = 500;
+
+    price = price * 1.1;
+
+    printf("%f",(float)price);
+}
