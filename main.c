8c018b6f24662ef6afdf0981ac092e77c912403d
honokk <132962467+honokk@users.noreply.github.com>
2023-11-17 14:02:01 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+    int a = 20000, b = 300, c = 1;
+
+    printf("Aは%5dです\n",a);
+    printf("Bは%5dです\n",b);
+    printf("Cは%5dです\n",c);
+    
+    return 0;
+     //comment
+}
